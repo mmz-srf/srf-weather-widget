54cbbed699fddc6f1635acc245d20cadceb123dc
Manuel Roth <manuelroth@hotmail.ch>
2023-09-28 14:03:06 +0200

    Add icons

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,12 +1,24 @@
 <script>
   import TailwindCss from "./lib/TailwindCSS.svelte";
+
+  async function getData() {
+    const response = await fetch(
+      "https://www.srf.ch/meteoapi/forecastpoint/47.3903,9.2775"
+    );
+    if (response.ok) {
+      const data = await response.json();
+
+      console.log(data);
+
+      return data;
+    }
+  }
 </script>
 
 <TailwindCss />
-<main class="red">
-  <h1 class="py-8 px-4 border border-indigo-900 shadow-lg">Hello {name}!</h1>
-  <p>
-    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
-    how to build Svelte apps.
-  </p>
+<main>
+  {#await getData() then data}
+    <h2>{data.geolocation.default_name}</h2>
+    <h3>{data.days[0].TN_C} | {data.days[0].TX_C}</h3>
+  {/await}
 </main>
