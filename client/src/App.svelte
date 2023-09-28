02a17f553f3e8ac4a526962449f0a59bd9ecd03c
Manuel Roth <manuelroth@hotmail.ch>
2023-09-28 11:31:05 +0200

    Setup client using svelte, tailwind and vitejs

---
+++ client/src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+  import TailwindCss from "./lib/TailwindCSS.svelte";
+</script>
+
+<TailwindCss />
+<main class="red">
+  <h1 class="py-8 px-4 border border-indigo-900 shadow-lg">Hello {name}!</h1>
+  <p>
+    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+    how to build Svelte apps.
+  </p>
+</main>
