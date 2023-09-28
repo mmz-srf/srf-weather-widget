25027f7671fc2bc969441afcdc20a045fb260561
Manuel Roth <manuelroth@hotmail.ch>
2023-09-28 15:00:59 +0200

    Set assetBase

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 
+const assetBase = process.env.VITE_ASSET_BASE || "./";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
   build: {
-    assetsDir: "./"
+    assetsDir: assetBase
   }
 });
