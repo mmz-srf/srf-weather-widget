95f045927c98fef1f56f153b1997f40f35a4488b
Manuel Roth <manuelroth@hotmail.ch>
2023-09-28 15:09:19 +0200

    Try to fix build

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,5 @@ const assetBase = process.env.VITE_ASSET_BASE || "./";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  build: {
-    assetsDir: assetBase
-  }
+  base: "./"
 });
