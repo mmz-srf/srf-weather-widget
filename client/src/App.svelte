bcc81a9e48b6970fcb9eba62990e4837b3a4705b
Manuel Roth <manuelroth@hotmail.ch>
2023-09-28 14:37:45 +0200

    add icons

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,9 +1,132 @@
 <script>
   import TailwindCss from "./lib/TailwindCSS.svelte";
+  import NightIcon1 from "./assets/icons/color/-1.svg";
+  import NightIcon10 from "./assets/icons/color/-10.svg";
+  import NightIcon11 from "./assets/icons/color/-11.svg";
+  import NightIcon12 from "./assets/icons/color/-12.svg";
+  import NightIcon13 from "./assets/icons/color/-13.svg";
+  import NightIcon14 from "./assets/icons/color/-14.svg";
+  import NightIcon15 from "./assets/icons/color/-15.svg";
+  import NightIcon16 from "./assets/icons/color/-16.svg";
+  import NightIcon17 from "./assets/icons/color/-17.svg";
+  import NightIcon18 from "./assets/icons/color/-18.svg";
+  import NightIcon19 from "./assets/icons/color/-19.svg";
+  import NightIcon2 from "./assets/icons/color/-2.svg";
+  import NightIcon20 from "./assets/icons/color/-20.svg";
+  import NightIcon21 from "./assets/icons/color/-21.svg";
+  import NightIcon22 from "./assets/icons/color/-22.svg";
+  import NightIcon23 from "./assets/icons/color/-23.svg";
+  import NightIcon24 from "./assets/icons/color/-24.svg";
+  import NightIcon25 from "./assets/icons/color/-25.svg";
+  import NightIcon26 from "./assets/icons/color/-26.svg";
+  import NightIcon27 from "./assets/icons/color/-27.svg";
+  import NightIcon28 from "./assets/icons/color/-28.svg";
+  import NightIcon29 from "./assets/icons/color/-29.svg";
+  import NightIcon3 from "./assets/icons/color/-3.svg";
+  import NightIcon30 from "./assets/icons/color/-30.svg";
+  import NightIcon4 from "./assets/icons/color/-4.svg";
+  import NightIcon5 from "./assets/icons/color/-5.svg";
+  import NightIcon6 from "./assets/icons/color/-6.svg";
+  import NightIcon7 from "./assets/icons/color/-7.svg";
+  import NightIcon8 from "./assets/icons/color/-8.svg";
+  import NightIcon9 from "./assets/icons/color/-9.svg";
+  import Icon1 from "./assets/icons/color/1.svg";
+  import Icon10 from "./assets/icons/color/10.svg";
+  import Icon11 from "./assets/icons/color/11.svg";
+  import Icon12 from "./assets/icons/color/12.svg";
+  import Icon13 from "./assets/icons/color/13.svg";
+  import Icon14 from "./assets/icons/color/14.svg";
+  import Icon15 from "./assets/icons/color/15.svg";
+  import Icon16 from "./assets/icons/color/16.svg";
+  import Icon17 from "./assets/icons/color/17.svg";
+  import Icon18 from "./assets/icons/color/18.svg";
+  import Icon19 from "./assets/icons/color/19.svg";
+  import Icon2 from "./assets/icons/color/2.svg";
+  import Icon20 from "./assets/icons/color/20.svg";
+  import Icon21 from "./assets/icons/color/21.svg";
+  import Icon22 from "./assets/icons/color/22.svg";
+  import Icon23 from "./assets/icons/color/23.svg";
+  import Icon24 from "./assets/icons/color/24.svg";
+  import Icon25 from "./assets/icons/color/25.svg";
+  import Icon26 from "./assets/icons/color/26.svg";
+  import Icon27 from "./assets/icons/color/27.svg";
+  import Icon28 from "./assets/icons/color/28.svg";
+  import Icon29 from "./assets/icons/color/29.svg";
+  import Icon3 from "./assets/icons/color/3.svg";
+  import Icon30 from "./assets/icons/color/30.svg";
+  import Icon4 from "./assets/icons/color/4.svg";
+  import Icon5 from "./assets/icons/color/5.svg";
+  import Icon6 from "./assets/icons/color/6.svg";
+  import Icon7 from "./assets/icons/color/7.svg";
+  import Icon8 from "./assets/icons/color/8.svg";
+  import Icon9 from "./assets/icons/color/9.svg";
+
+  const iconMap = {
+    "-1": NightIcon1,
+    "-10": NightIcon10,
+    "-11": NightIcon11,
+    "-12": NightIcon12,
+    "-13": NightIcon13,
+    "-14": NightIcon14,
+    "-15": NightIcon15,
+    "-16": NightIcon16,
+    "-17": NightIcon17,
+    "-18": NightIcon18,
+    "-19": NightIcon19,
+    "-2": NightIcon2,
+    "-20": NightIcon20,
+    "-21": NightIcon21,
+    "-22": NightIcon22,
+    "-23": NightIcon23,
+    "-24": NightIcon24,
+    "-25": NightIcon25,
+    "-26": NightIcon26,
+    "-27": NightIcon27,
+    "-28": NightIcon28,
+    "-29": NightIcon29,
+    "-3": NightIcon3,
+    "-30": NightIcon30,
+    "-4": NightIcon4,
+    "-5": NightIcon5,
+    "-6": NightIcon6,
+    "-7": NightIcon7,
+    "-8": NightIcon8,
+    "-9": NightIcon9,
+    "1": Icon1,
+    "10": Icon10,
+    "11": Icon11,
+    "12": Icon12,
+    "13": Icon13,
+    "14": Icon14,
+    "15": Icon15,
+    "16": Icon16,
+    "17": Icon17,
+    "18": Icon18,
+    "19": Icon19,
+    "2": Icon2,
+    "20": Icon20,
+    "21": Icon21,
+    "22": Icon22,
+    "23": Icon23,
+    "24": Icon24,
+    "25": Icon25,
+    "26": Icon26,
+    "27": Icon27,
+    "28": Icon28,
+    "29": Icon29,
+    "3": Icon3,
+    "30": Icon30,
+    "4": Icon4,
+    "5": Icon5,
+    "6": Icon6,
+    "7": Icon7,
+    "8": Icon8,
+    "9": Icon9
+  };
 
   async function getData() {
     const response = await fetch(
-      "https://www.srf.ch/meteoapi/forecastpoint/47.3903,9.2775"
+      "https://www.srf.ch/meteoapi/forecastpoint/-90.0000,0.0000"
     );
     if (response.ok) {
       const data = await response.json();
@@ -20,5 +143,6 @@
   {#await getData() then data}
     <h2>{data.geolocation.default_name}</h2>
     <h3>{data.days[0].TN_C} | {data.days[0].TX_C}</h3>
+    <img src={iconMap[data.days[0].symbol_code.toString()]} alt="" />
   {/await}
 </main>
