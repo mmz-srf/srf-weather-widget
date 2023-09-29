d4648e38ac70035949b69b2278b01ddbb52cd7bb
Manuel Roth <manuelroth@hotmail.ch>
2023-09-29 10:22:45 +0200

    Make sure entry point assets dont contain content hash

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,20 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: "[name].js",
+        assetFileNames: (chunk) => {
+          if (chunk.name.includes("css")) {
+            return "[name].[ext]";
+          } else {
+            return "assets/[name]-[hash].[ext]";
+          }
+        }
+      }
+    }
+  },
   plugins: [svelte()],
   base: "./"
 });
