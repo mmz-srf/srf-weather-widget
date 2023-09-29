f4f056247a913660fc0f80d95b710189379ef7a2
Markus Unterkofler <157289+munterkofler@users.noreply.github.com>
2023-09-29 11:46:43 +0200

    Add data attributes

--- wp-plugin/srf-weather-widget/meteo.php
+++ wp-plugin/srf-weather-widget/meteo.php
@@ -14,12 +14,21 @@ function meteo_widget($atts = [], $content = null, $tag = '') {
     $widgetAtts = shortcode_atts(
         [
             'size' => 'S',
+            'geolocation' => '',
+            'locationname' => '',
         ],
         array_change_key_case( (array) $atts, CASE_LOWER ),
         $tag
     );
 
-    return '<div class="srf-weather-widget" data-size="'.esc_html($widgetAtts['size']).'"></div>';
+    if (false === in_array($widgetAtts['size'], ['S', 'M', 'L'])) {
+        return 'Not a valid widget size set.';
+    }
+
+    return '<div class="srf-weather-widget" 
+        data-size="'.esc_html($widgetAtts['size']).'" 
+        data-geolocation="'.esc_html($widgetAtts['geolocation']).'" 
+        data-location-name="'.esc_html($widgetAtts['locationname']).'"></div>';
 }
 
 add_action( 'wp_head', 'meteo_resources' );
