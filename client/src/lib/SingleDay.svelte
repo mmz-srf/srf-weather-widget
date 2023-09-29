dd698681ea03a21f6b3571f575d2f0c53f8867dd
Santiago Cepeda <smcepeda@gmail.com>
2023-09-29 16:36:44 +0200

    Create SingleDay component

---
+++ client/src/lib/SingleDay.svelte
@@ -0,0 +1,17 @@
+<script>
+  import WeatherIcons from "./WeatherIcons.svelte";
+
+  export let dailyTemp;
+  export let symbol;
+  export let dimensions;
+  export let currentTime;
+
+  const isNow = currentTime === "Jetzt";
+  const fontWeight = isNow ? "font-bold" : "font-light";
+</script>
+
+<div class="flex flex-col items-center">
+  <div class={fontWeight}>{currentTime}</div>
+  <WeatherIcons {symbol} {dimensions} />
+  <div>{dailyTemp}°</div>
+</div>
