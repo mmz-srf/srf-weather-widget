59c0d8b2e2ccd0b88990df75366edf25379c524f
Markus Unterkofler <157289+munterkofler@users.noreply.github.com>
2023-09-29 10:43:52 +0200

    Fix plugin

--- wp-plugin/srf-weather-widget/meteo.php
+++ wp-plugin/srf-weather-widget/meteo.php
@@ -12,12 +12,12 @@ Plugin Name: Meteo
 
 // We need some CSS to position the paragraph.
 function meteo_resources() {
-    echo '<script src="https://mmz-srf.github.io/srf-weather-widget/index.js" type="text/javascript"></script>';
+    echo '<script src="https://mmz-srf.github.io/srf-weather-widget/index.js" type="module"></script>';
     echo '<link rel="stylesheet" href="https://mmz-srf.github.io/srf-weather-widget/index.css">';
 }
 
 function meteo_widget() {
-    return '<div id="app"></div>';
+    return '<div class="srf-weather-widget"></div>';
 }
 
 add_action( 'wp_head', 'meteo_resources' );
