18350acd2ebf14a6c40f7d1730fdf3a6d4a8dea5
Santiago Cepeda <smcepeda@gmail.com>
2023-09-29 14:04:42 +0200

    Create WeatherOverview component

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,12 +1,8 @@
 <script>
   import { onMount } from "svelte";
   import TailwindCss from "./lib/TailwindCSS.svelte";
-  import WeatherIcons from "./lib/WeatherIcons.svelte";
-
+  import WeatherOverview from "./lib/WeatherOverview.svelte";
   export let size;
-  export let geolocation;
-  export let locationName;
-
   let weatherData = null;
 
   async function fetchData() {
@@ -22,28 +18,15 @@
       console.error("Error fetching data:", error);
     }
   }
-
   onMount(() => {
     fetchData();
   });
 </script>
 
 <TailwindCss />
-<main class="flex justify-center">
+<main class="flex justify-center mt-2">
   {#if weatherData}
-    <div
-      class="flex flex-row items-center justify-between w-full max-w-sm text-white bg-blue-500 border-2 rounded-md border-cyan-900"
-    >
-      <div class="flex flex-col my-4 ml-6 space-y-1">
-        <h2 class="text-lg">{weatherData.geolocation.default_name}</h2>
-        <h3 class="text-xl font-bold">
-          {weatherData.days[0].TN_C} | {weatherData.days[0].TX_C}
-        </h3>
-      </div>
-      <div class="my-4 mr-4">
-        <WeatherIcons symbol={weatherData.days[0].symbol_code.toString()} />
-      </div>
-    </div>
+    <WeatherOverview {weatherData} {size} />
   {:else}
     <p>Loading...</p>
   {/if}
