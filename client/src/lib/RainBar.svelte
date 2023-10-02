dfad9f61341a37626046f6639058358d3c1326b2
Santiago Cepeda <smcepeda@gmail.com>
2023-10-02 14:29:59 +0200

    Refactor dark mode implementation

--- client/src/lib/RainBar.svelte
+++ client/src/lib/RainBar.svelte
@@ -1,14 +1,13 @@
 <script>
   export let rainInMM;
 
-  let useDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
-  $: useDarkMode;
+  $: isDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
 </script>
 
 <div class="h-[30px] w-full flex flex-col-reverse items-center relative mt-2">
   <div
     class="absolute w-full h-full"
-    style={useDarkMode
+    style={isDarkMode
       ? `background: linear-gradient(180deg, rgb(8, 81, 139), rgb(8, 81, 139) 1px, transparent 1%, transparent); background-size: 100% 5px;`
       : `background: linear-gradient(180deg, rgba(0, 64, 114, 1), rgba(0, 64, 114, 1) 1px, transparent 1%, transparent); background-size: 100% 5px;`}
   />
