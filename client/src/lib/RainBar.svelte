1582f97758c62e0d8055b6295e08516bc6749318
Santiago Cepeda <smcepeda@gmail.com>
2023-10-02 14:48:01 +0200

    Change rainBar colors

--- client/src/lib/RainBar.svelte
+++ client/src/lib/RainBar.svelte
@@ -8,8 +8,8 @@
   <div
     class="absolute w-full h-full"
     style={isDarkMode
-      ? `background: linear-gradient(180deg, rgb(8, 81, 139), rgb(8, 81, 139) 1px, transparent 1%, transparent); background-size: 100% 5px;`
-      : `background: linear-gradient(180deg, rgba(0, 64, 114, 1), rgba(0, 64, 114, 1) 1px, transparent 1%, transparent); background-size: 100% 5px;`}
+      ? `background: linear-gradient(180deg, rgba(0, 64, 114, 1), rgba(0, 64, 114, 1) 1px, transparent 1%, transparent); background-size: 100% 5px;`
+      : `background: linear-gradient(180deg, rgba(11, 95, 161, 1), rgba(11, 95, 161, 1) 1px, transparent 1%, transparent); background-size: 100% 5px;`}
   />
   <div
     class="bg-sky-500 w-1/2 max-h-8 absolute bottom-[4px]"
