5892fc9514e99dee3c2eb675e9306c7b9cb35e7b
Manuel Roth <manuelroth@hotmail.ch>
2023-10-02 11:50:50 +0200

    Design adjustment rain bar

--- client/src/lib/RainBar.svelte
+++ client/src/lib/RainBar.svelte
@@ -2,7 +2,7 @@
   export let rainInMM;
 </script>
 
-<div class="h-[30px] w-full flex flex-col-reverse items-center relative">
+<div class="h-[30px] w-full flex flex-col-reverse items-center relative mt-2">
   <div
     class="absolute h-full w-full"
     style={`background: linear-gradient(180deg, rgba(11, 95, 161, 1), rgba(11, 95, 161, 1) 1px, transparent 1%, transparent); background-size: 100% 5px;`}
