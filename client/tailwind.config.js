5272ebb20aa4b010e4030bad8a8ac5ad3f86f4d3
Manuel Roth <manuelroth@hotmail.ch>
2023-10-02 16:55:49 +0200

    Optimize tailwindcss config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,18 +1,14 @@
 export default {
+  content: ["./index.html", "./src/**/*.{svelte,js}"],
   plugins: [],
   theme: {
     extend: {
       colors: {
         widget: {
           dark: "#08518B",
-          light: "#0A67B0",
-        },
-      },
-    },
-  },
-  purge: ["./index.html", "./src/**/*.{svelte,js,ts}"], //for unused css
-  variants: {
-    extend: {},
-  },
-  darkmode: false, // or 'media' or 'class'
+          light: "#0A67B0"
+        }
+      }
+    }
+  }
 };
